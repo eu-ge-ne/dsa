0d33b3469141e4a090ba0a23ed526719e1105588
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-05-03 16:38:46 -0400

    1

--- src/insertion-sort/index.ts
+++ src/insertion-sort/index.ts
@@ -4,10 +4,13 @@ export function insertionSort<T>(arr: T[]): void {
   for (let i = 1; i < l; i += 1) {
     const key = arr[i]!;
 
-    let j = i - 1;
-    while ((j >= 0) && (arr[j]! > key)) {
+    let j;
+    for (j = i - 1; j >= 0; j -= 1) {
+      if (arr[j]! <= key) {
+        break;
+      }
+
       arr[j + 1] = arr[j]!;
-      j = j - 1;
     }
 
     arr[j + 1] = key;
